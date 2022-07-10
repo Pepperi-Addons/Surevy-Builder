b0af2da8f1f8fdb0ae8ce310d299a8b10e6c6af5
tomerp_pepperi <tomer.p@pepperi.com>
2022-07-10 19:24:53 +0300

    no message

--- client-side/webpack.config.js
+++ client-side/webpack.config.js
@@ -1,20 +1,3 @@
-// const { shareAll, share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
-// const filename = 'survey_builder';
-
-// module.exports = withModuleFederationPlugin({
-//     name: filename,
-//     filename: `${filename}.js`,
-//     exposes: {
-//         './SettingsModule': './src/app/components/settings/index.ts',
-//         './SurveyBuilderModule': './src/app/components/survey-builder/index.ts'
-//     },
-//     shared: {
-//         ...shareAll({ strictVersion: true, requiredVersion: 'auto' }),
-//     }
-// });
-
-
-
 const { shareAll, share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 const filename = 'survey_builder';
 
