eab210676e1ff7da0ca1163b2aabb3881d99e444
tomerp_pepperi <tomer.p@pepperi.com>
2022-07-24 18:27:44 +0300

    no message

--- client-side/src/app/components/sequence-number/sequence-number.module.ts
+++ client-side/src/app/components/sequence-number/sequence-number.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PepNgxLibModule, PepAddonService } from '@pepperi-addons/ngx-lib';
 import { PepIconModule, PepIconRegistry, pepIconSystemMove} from '@pepperi-addons/ngx-lib/icon';
 
-import { TranslateLoader, TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { SequenceNumberComponent } from './sequence-number.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,11 +29,8 @@ const pepIcons = [
 export class SequenceNumberModule { 
 
 constructor(
-    translate: TranslateService,
-    private pepIconRegistry: PepIconRegistry,
-    private pepAddonService: PepAddonService
+    private pepIconRegistry: PepIconRegistry
     ) {
-        this.pepAddonService.setDefaultTranslateLang(translate);
         this.pepIconRegistry.registerIcons(pepIcons);
     }
 }
