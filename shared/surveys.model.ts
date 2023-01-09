82dba93ab37a59fe3138f8e7047d41218cb0a123
tomerp_pepperi <tomer.p@pepperi.com>
2023-01-09 17:52:26 +0200

    - Update model from papi-sdk

--- shared/surveys.model.ts
+++ shared/surveys.model.ts
@@ -36,29 +36,6 @@ export interface SurveyTemplate extends AddonData {
     StatusName?: SurveyStatusType;
 }
 
-// Temp need to remove it when will be integrated with papi-sdk
-export interface Survey extends AddonData {
-    // status keyword values (not free text)
-    StatusName?:string;
-    // unique ID - ?
-    ExternalID?:string;
-    // reference to the SurveyTemplate
-    Template:string;
-    // contains object
-    Answers?:Answer[];
-    // the UUID of the user who created the survey
-    // mandatory
-    Creator?:string;
-    // the UUID of the account the survey was created for
-    // mandatory??
-    Account?:string;
-    // // the UUID of the user who last updated the survey
-    // Performer: string;
-}
-export interface Answer {
-    Key: string;
-    Answer: any;
-}
 
 export interface SurveyTemplateDateRange {
     From?: string;
